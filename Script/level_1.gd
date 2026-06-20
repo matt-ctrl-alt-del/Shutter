be5516dabf861cf3250e1961fbecdf86e8e2544f
matt-ctrl-alt-del <vmatthewdaniel@gmail.com>
2026-06-20 11:29:34 -0700

    Working on joystick

--- Script/level_1.gd
+++ Script/level_1.gd
@@ -9,3 +9,9 @@ func _ready() -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	$Fade_transition/AnimationPlayer.play("fade_in")
 	get_tree().change_scene_to_file("res://Scenes/level_2.tscn")
+
+
+func _on_texture_button_pressed() -> void:
+	Input.action_press("jump")
+	Input.action_release("jump")
+	
