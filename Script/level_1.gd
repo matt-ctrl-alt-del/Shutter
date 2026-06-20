ea3d92609a3b125b7a6bf53be3fe7528762e25b2
matt-ctrl-alt-del <vmatthewdaniel@gmail.com>
2026-06-20 00:34:06 -0700

    Level 1
    Added Map for level 1 and then added character and more sprites

---
+++ Script/level_1.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	get_tree().change_scene_to_file("res://Scenes/level_2.tscn")
--- Script/lvl_2_eureka.gd
+++ Script/lvl_2_eureka.gd
@@ -8,15 +8,11 @@ const JUMP_VELOCITY = -400.0
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		pass
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
